e145960f5385a8a855341e200099fcb8d5550af8
Colin Leung <colin@digi3studio.com>
2019-01-21 13:53:40 +0800

    first commit

---
+++ public/media/js/config/site.jsx
@@ -0,0 +1,11 @@
+export default {
+  paths : {
+    "jxon"             : "//unpkg.com/jxon@2.0.0-beta.5/jxon.min",
+    "react"            : "//unpkg.com/react@16.7.0/umd/react.development",
+    "react-dom"        : "//unpkg.com/react-dom@16.7.0/umd/react-dom.development",
+    'ufo-kit'          : "../../vendors/ufo-kit/ufo-kit",
+  },
+  shim: {
+
+  },
+}
